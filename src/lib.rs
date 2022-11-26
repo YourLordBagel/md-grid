aa6be61bde0b567c6007e10ce624a9c1a0e44538
YourLordBagel <frigulettopeter@gmail.com>
2022-11-25 22:35:45 -0600

    Init Commit

---
+++ src/lib.rs
@@ -0,0 +1,133 @@
+use std::{error::Error, fmt::Debug};
+
+#[derive(Debug)]
+pub struct Grid<T: Clone> {
+    grid: Vec<T>,
+    axes: usize,
+    dimensions: Vec<usize>,
+}
+
+impl<T: Clone + Debug> Grid<T> {
+    pub fn new(default_value: T, dimensions: Vec<usize>) -> Self {
+        let axes = dimensions.len();
+
+        let size = dimensions.iter().product();
+
+        let mut grid = Vec::with_capacity(size);
+        for _ in 0..size {
+            grid.push(default_value.clone());
+        }
+
+        Self {
+            grid,
+            axes,
+            dimensions,
+        }
+    }
+
+    pub fn get(&self, target: Vec<usize>) -> Result<&T, Box<dyn Error>> {
+        let target = self.translate_index(target)?;
+        let val = &self.grid[target];
+        Ok(val)
+    }
+
+    pub fn set(&mut self, target: Vec<usize>, val: T) -> Result<(), Box<dyn Error>> {
+        let target = self.translate_index(target)?;
+        self.grid[target] = val;
+        Ok(())
+    }
+
+    fn translate_index(&self, target: Vec<usize>) -> Result<usize, Box<dyn Error>> {
+        if target.len() != self.axes {
+            return Err(format!(
+                "ERROR: Tried to index with {} dimensions when grid only has {} dimensions",
+                target.len(),
+                &self.axes
+            )
+            .into());
+        }
+
+        let mut index = 0;
+        for (i, v) in target.iter().enumerate() {
+            let step = v * &self.dimensions.iter().skip(i + 1).product();
+            index += step;
+        }
+
+        if index >= self.grid.len() {
+            return Err(format!(
+                "ERROR: Index ({}) out of bounds ({})",
+                index,
+                self.grid.len()
+            )
+            .into());
+        }
+
+        Ok(index)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_grid() {
+        let grid = Grid::new("default_value", vec![5, 5]);
+        // Test grid size
+        assert_eq!(grid.grid.len(), 25);
+        // Test defualt values
+        for i in grid.grid.iter() {
+            assert_eq!(*i, "default_value");
+        }
+    }
+
+    #[test]
+    fn translate_index() {
+        // 2d grid (10x10)
+        let mut grid = Grid::new(0, vec![10, 10]);
+
+        grid.set(vec![5, 9], 5).unwrap();
+        grid.set(vec![0, 5], 32).unwrap();
+        grid.set(vec![5, 0], 25).unwrap();
+        grid.set(vec![9, 9], 56).unwrap();
+        grid.set(vec![0, 0], 7).unwrap();
+
+        assert_eq!(grid.grid[59], 5);
+        assert_eq!(grid.grid[5], 32);
+        assert_eq!(grid.grid[50], 25);
+        assert_eq!(grid.grid[99], 56);
+        assert_eq!(grid.grid[0], 7);
+
+        // 3d grid (10x10x10)
+        let mut grid = Grid::new(0, vec![10, 10, 10]);
+
+        grid.set(vec![3, 7, 6], 12).unwrap();
+        grid.set(vec![0, 4, 3], 23).unwrap();
+        grid.set(vec![5, 0, 7], 32).unwrap();
+        grid.set(vec![4, 6, 0], 63).unwrap();
+        grid.set(vec![9, 9, 9], 87).unwrap();
+        grid.set(vec![0, 0, 0], 34).unwrap();
+
+        assert_eq!(grid.grid[376], 12);
+        assert_eq!(grid.grid[43], 23);
+        assert_eq!(grid.grid[507], 32);
+        assert_eq!(grid.grid[460], 63);
+        assert_eq!(grid.grid[999], 87);
+        assert_eq!(grid.grid[0], 34);
+
+        // 4d grid (10x10x10x10)
+        let mut grid = Grid::new(0, vec![10, 10, 10, 10]);
+
+        grid.set(vec![5, 3, 7, 9], 20).unwrap();
+        grid.set(vec![9, 9, 9, 9], 24).unwrap();
+        grid.set(vec![0, 0, 0, 0], 10).unwrap();
+
+        assert_eq!(grid.grid[5379], 20);
+        assert_eq!(grid.grid[9999], 24);
+        assert_eq!(grid.grid[0], 10);
+
+        // Test default values
+        assert_eq!(grid.grid[8654], 0);
+        assert_eq!(grid.grid[23], 0);
+    }
+}
